2c68f0ee80c8c4ada8590c565b0618dfd8433a4d
Your Name <your.email@example.com>
2025-07-18 15:50:28 +0300

    Updated the upload function

--- routes/upload.ts
+++ routes/upload.ts
@@ -3,6 +3,6 @@ import { uploadImageController } from '../controllers/upload/imageUpload';
 
 const router = express.Router();
 
-router.post('/image', uploadImageController[0] as express.RequestHandler, uploadImageController[1]);
+router.post('/image',uploadImageController[0] as express.RequestHandler, uploadImageController[1]);
 
 export default router;
